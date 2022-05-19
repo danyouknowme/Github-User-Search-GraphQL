56d5e676728b20ad39b1786bc30bb58bf07374e8
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-05-20 00:07:47 +0700

    Fixed user query

--- src/client/query.ts
+++ src/client/query.ts
@@ -3,16 +3,27 @@ import { gql } from '@apollo/client';
 export const USER_QUERY = gql`
 query User($username: String!) { 
     user(login: $username) { 
-      name 
-      email 
-      company 
-      bio 
+      name
+      email
+      company
+      bio
+      avatarUrl
       followers { 
         totalCount
       } 
       following { 
         totalCount 
-      } repositories(last: 50, isFork: false) { nodes { name url stargazerCount primaryLanguage { id name } } } } 
+      } repositories(last: 20, isFork: false) {
+        nodes { 
+          name 
+          url 
+          primaryLanguage { 
+            id 
+            name 
+          } 
+        } 
+      } 
+    } 
   }
 `;
 
