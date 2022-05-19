6340e90be56b3af4b5f68363024b0841771af7cf
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-05-20 00:07:36 +0700

    Add interface of user and card profile props

--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,21 @@ export interface NotifyProps {
     message: string;
     active: boolean;
 }
+
+export interface CardProfileProps {
+    user: User;
+}
+
+export interface User {
+    name: string;
+    email?: string;
+    company?: string;
+    bio?: string;
+    avatarUrl?: string;
+    followers: {
+        totalCount: number;
+    };
+    following: {
+        totalCount: number;
+    };
+}
